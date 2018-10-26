4b73b0213b24b4c7d4e9b17735a7f88ecb29ff6a
gaga69 <yumm.yumm.08feb@gmail.com>
2018-10-26 21:30:59 +0900

    Add flash messsage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+    def bulma_class_for(flash_type)
+        case flash_type
+        when :notice
+            "is-success"
+        when :alert
+            "is-warning"
+        else
+            flash_type.to_s
+        end
+    end
 end
