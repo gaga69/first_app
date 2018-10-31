b00b6c59c8d90cd5378b32f706a2c9ce1cf7e91f
gaga69 <yumm.yumm.08feb@gmail.com>
2018-10-31 22:16:33 +0900

    Add the Post Model

---
+++ db/migrate/20181031130709_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :book_title
+      t.string :book_author
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_index :posts, [:user_id, :created_at]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_122404) do
+ActiveRecord::Schema.define(version: 2018_10_31_130709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "book_title"
+    t.string "book_author"
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
