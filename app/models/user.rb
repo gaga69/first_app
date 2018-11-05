1d44dc75c65c2f855b5412df986e1aeb076cbab2
gaga69 <yumm.yumm.08feb@gmail.com>
2018-11-05 22:48:47 +0900

    Make a Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationships", dependency: :destroy
+  has_many :followers, through: :reverse_relationships
+
   validates :name, presence: true, length: { maximum: 15 }
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def following!(other_user)
+    relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
